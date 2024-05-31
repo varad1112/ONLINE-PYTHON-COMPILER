812217dde7a8ec451e6e3bf3305b4187e3349aca
varad1112 <61153710+varad1112@users.noreply.github.com>
2024-05-31 18:51:26 +0530

    Add files via upload

---
+++ Login/index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if(!isset($_SESSION["username"])){
+header("Location: login.php");
+exit(); }
+?>
+<html>
+<a href="http://localhost/python_pyd/index.php"></a>
+</html>
+
+
